c87005e379f27fe997f73311ed1bf9daaba88c65
Sohee <sh11govlday@gmail.com>
2024-11-24 15:33:56 +0900

    feat : 기본 구분자 기준으로 숫자 분리 및 결과 출력 기능 추가

--- src/App.js
+++ src/App.js
@@ -6,7 +6,14 @@ class App {
       const input = await MissionUtils.Console.readLineAsync(
         '덧셈할 문자열을 입력해 주세요.\n'
       );
-      MissionUtils.Console.print(input);
+
+      const numbers = input.split(/,|:/).map(Number);
+
+      let result = 0;
+      for (const number of numbers) {
+        result += number;
+      }
+      MissionUtils.Console.print(`결과 : ${result}`);
     } catch (error) {}
   }
 }
