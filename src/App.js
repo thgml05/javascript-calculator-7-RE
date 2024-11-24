8c5ff3a893c8202cf3efaa68944b3cf63dbb84ff
Sohee <sh11govlday@gmail.com>
2024-11-24 14:32:10 +0900

    feat : 문자열 입력 기능 추가

--- src/App.js
+++ src/App.js
@@ -1,5 +1,14 @@
+import { MissionUtils } from '@woowacourse/mission-utils';
+
 class App {
-  async run() {}
+  async run() {
+    try {
+      const input = await MissionUtils.Console.readLineAsync(
+        '덧셈할 문자열을 입력해 주세요.\n'
+      );
+      MissionUtils.Console.print(input);
+    } catch (error) {}
+  }
 }
 
 export default App;
