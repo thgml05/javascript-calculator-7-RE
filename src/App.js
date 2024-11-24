0612f725eb08d4798668bc2f352ca53b7e30cf13
Sohee <sh11govlday@gmail.com>
2024-11-24 15:37:37 +0900

    refactor : 함수 분리

--- src/App.js
+++ src/App.js
@@ -3,18 +3,29 @@ import { MissionUtils } from '@woowacourse/mission-utils';
 class App {
   async run() {
     try {
-      const input = await MissionUtils.Console.readLineAsync(
-        '덧셈할 문자열을 입력해 주세요.\n'
-      );
-
-      const numbers = input.split(/,|:/).map(Number);
-
-      let result = 0;
-      for (const number of numbers) {
-        result += number;
-      }
+      const input = await this.getInput();
+      const numbers = this.splitNumbers(input);
+      const result = this.calculateResult(numbers);
       MissionUtils.Console.print(`결과 : ${result}`);
     } catch (error) {}
+  }
+
+  async getInput() {
+    return await MissionUtils.Console.readLineAsync(
+      '덧셈할 문자열을 입력해 주세요.\n'
+    );
+  }
+
+  splitNumbers(input) {
+    return input.split(/,|:/).map(Number);
+  }
+
+  calculateResult(numbers) {
+    let result = 0;
+    for (const number of numbers) {
+      result += number;
+    }
+    return result;
   }
 }
 
