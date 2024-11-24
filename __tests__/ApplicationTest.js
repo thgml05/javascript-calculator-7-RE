4139e1828c98e5f93bfbb2920a6d2f52742dd054
Sohee <sh11govlday@gmail.com>
2024-11-24 15:53:32 +0900

    test : 기본 구분자 테스트 코드 추가

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -16,17 +16,29 @@ const getLogSpy = () => {
   return logSpy;
 };
 
+const runTest = async (inputs, outputs) => {
+  mockQuestions(inputs);
+
+  const logSpy = getLogSpy();
+
+  const app = new App();
+  await app.run();
+
+  expect(logSpy).toHaveBeenCalledWith(outputs);
+};
+
 describe('문자열 계산기', () => {
-  test('문자열 입력 테스트', async () => {
-    const inputs = ['1,2,3'];
-    mockQuestions(inputs);
+  describe('기본 구분자 사용', () => {
+    test('쉼표(,) 구분자 테스트', async () => {
+      runTest(['1,2,3'], '결과 : 6');
+    });
 
-    const logSpy = getLogSpy();
-    const outputs = '1,2,3';
+    test('세미콜론(:) 구분자 테스트', async () => {
+      runTest(['1:2:3'], '결과 : 6');
+    });
 
-    const app = new App();
-    await app.run();
-
-    expect(logSpy).toHaveBeenCalledWith(outputs);
+    test('쉼표(,) & 세미콜론(:) 구분자 테스트', async () => {
+      runTest(['1,2:3'], '결과 : 6');
+    });
   });
 });
